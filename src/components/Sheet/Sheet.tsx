6a471e8b3c845b05568cc5fcee9d158f32e93bb2
Paul Michaut <119626732+PMcht@users.noreply.github.com>
2023-01-07 18:20:27 +0100

    Resizer implemented

--- src/components/Sheet/Sheet.tsx
+++ src/components/Sheet/Sheet.tsx
@@ -3,6 +3,7 @@ import { useRecoilValue } from 'recoil';
 import { SheetSizeState } from '../../store/SheetSizeState';
 import Cell, { Cell_height, Cell_width } from '../Cell/Cell';
 import Column from '../Column/Column';
+import Resiser from '../Resiser/Resiser';
 import Row from '../Row/Row';
 import classes from "./Sheet.module.css"
 
@@ -11,23 +12,26 @@ export type SheetProps<P = unknown> = P & { children?: ReactNode | undefined };
 const Sheet: FunctionComponent<SheetProps> = (props) => {
     const sheetSize = useRecoilValue(SheetSizeState);
 
-    const numberofRows = sheetSize.height/Cell_height;
-    const numberofColumns = sheetSize.width/Cell_width;
+    const numberofRows =Math.ceil(sheetSize.height/Cell_height);
+    const numberofColumns = Math.ceil(sheetSize.width/Cell_width);
 
     return (
-    <table className={classes.Grid}>
-        <tbody className={classes.Grid}>
-            {[...Array(numberofRows)].map((row, rowIndex) => (
-                <Row key={rowIndex}>
-                    {[...Array(numberofColumns)].map((column, columnIndex) => (
-                        <Column key={columnIndex}>
-                            <Cell cellId={`${rowIndex},${columnIndex}`} />
-                        </Column>
-                    ))}
-                </Row>
-           ))}
-        </tbody>
-    </table>
+    <div className={classes.SheetWrapper}>
+        <table className={classes.Grid}>
+            <tbody className={classes.Grid}>
+                {[...Array(numberofRows)].map((row, rowIndex) => (
+                    <Row key={rowIndex}>
+                        {[...Array(numberofColumns)].map((column, columnIndex) => (
+                            <Column key={columnIndex}>
+                                <Cell cellId={`${rowIndex},${columnIndex}`} />
+                            </Column>
+                        ))}
+                    </Row>
+            ))}
+            </tbody>
+        </table>
+        <Resiser />
+    </div>
     )
 }
 
